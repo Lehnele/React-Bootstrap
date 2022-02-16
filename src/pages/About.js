8ba0e7069b290c5016faf4e95b3b764bc0c8c135
Lehnele <danil.hap@mail.ru>
2022-02-16 14:45:10 +0300

    Alpha 1.0.1 a

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const About = () => {
     return (
         <div className='jumbotron'>
-            <div className='container'>
+            <div className='container text-white'>
                 <h1 className='display-4'>React-Bootstrap Github-Search</h1>
                 <p className="lead">Версия приложения: <strong>1.0.0</strong>.</p>
             </div>
